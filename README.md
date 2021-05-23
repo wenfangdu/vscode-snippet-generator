7c8476e418f12cea0b667ba5b6052ad5eeb7a704
Wenfang Du <no_reply_mail@163.com>
2021-05-23 09:27:27 +0800

    feat: support code => json convertion

---
+++ README.md
@@ -0,0 +1,12 @@
+# Snippet Generator
+
+[![Version](https://vsmarketplacebadge.apphb.com/version-short/wenfangdu.snippet-generator.svg)](https://marketplace.visualstudio.com/items?itemName=wenfangdu.snippet-generator)
+[![Rating](https://vsmarketplacebadge.apphb.com/rating-star/wenfangdu.snippet-generator.svg)](https://marketplace.visualstudio.com/items?itemName=wenfangdu.snippet-generator)
+
+> 📜 Snippet generator for VSCode
+
+[💿 Install Via Marketplace](https://marketplace.visualstudio.com/items?itemName=wenfangdu.snippet-generator)
+
+Command: `Generate Snippet`
+
+![Demo](https://raw.githubusercontent.com/wenfangdu/vscode-snippet-generator/main/images/demo.gif)
---
+++ extension.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const { commands, window, workspace, env } = require('vscode')
+
+exports.activate = ({ subscriptions }) => {
+  const disposable = commands.registerCommand(
+    'snippet-generator.generateSnippet',
+    async () => {
+      const editor = window.activeTextEditor
+
+      if (!editor) {
+        return window.showErrorMessage('No active text editor')
+      }
+
+      const selection = editor.document.getText(editor.selection)
+
+      if (!selection.length) {
+        return window.showErrorMessage('No selected text')
+      }
+
+      const name = await window.showInputBox({
+        placeHolder: 'Please enter name (required)',
+        validateInput: input => (input ? '' : 'Name is required'),
+      })
+
+      if (name === undefined) {
+        return
+      }
+
+      const scope = await window.showInputBox({
+        placeHolder: 'Please enter scope (optional)',
+      })
+
+      if (scope === undefined) {
+        return
+      }
+
+      const prefix = await window.showInputBox({
+        placeHolder: 'Please enter prefix (required)',
+        validateInput: input => (input ? '' : 'Prefix is required'),
+      })
+
+      if (prefix === undefined) {
+        return
+      }
+
+      const description = await window.showInputBox({
+        placeHolder: 'Please enter description (optional)',
+      })
+
+      if (description === undefined) {
+        return
+      }
+
+      const tabSize = workspace.getConfiguration('editor').get('tabSize')
+
+      const spaces = new RegExp(` {${tabSize}}`, 'g')
+
+      const snippetObj = {
+        [name]: {
+          ...(scope && { scope }),
+          prefix,
+          body: selection
+            .split(/\r?\n/)
+            .map(line => line.replace(spaces, '\t')),
+          ...(description && { description }),
+        },
+      }
+
+      const snippetJSON = JSON.stringify(snippetObj, null, tabSize)
+        .split('\n')
+        .slice(1, -1)
+        .join('\n')
+
+      env.clipboard.writeText(snippetJSON)
+
+      window.showInformationMessage(
+        'Snippet has been copied into the clipboard',
+      )
+    },
+  )
+
+  subscriptions.push(disposable)
+}
